903e7f26b264139b6d21abdd6bb94daa42d7efe8
TarrahNhari <63723421+TarrahNhari@users.noreply.github.com>
2020-04-15 14:13:35 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+let firstName;
+let lastName;
+let age = 28;
+let lightOn = true;
+
+function lightSwitch() {
+
+    if (lightOn == true) {
+        lightOn = false
+        console.log(lightOn)
+    }
+    else {
+        lightOn= true
+        console.log(lightOn)
+    }
+}
+lightSwitch()
+lightSwitch()
+lightSwitch()
+
+let numberOne;
+let numberTwo;
+let total;
+
+function addition(numberOne, numberTwo) {
+    total = numberOne + numberTwo
+    console.log(total)
+}
+
+addition(20, 30)
+addition(123, 56)
+addition(44,45)
